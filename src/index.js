c0aa709cb613f97151bebe929e4031ff9c9e9a5d
NisseBoman <nisse@tnfgruppen.com>
2023-03-22 15:10:16 +0100

    Dumping for the fun of it :D

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 //! Default Compute@Edge template program.
-
 /// <reference types="@fastly/js-compute" />
-import indexPage from "./index.html";
+
+// import { CacheOverride } from "fastly:cache-override";
+// import { Logger } from "fastly:logger";
+import { includeBytes } from "fastly:experimental";
+import { get } from "http";
+
+// Load a static file as a Uint8Array at compile time.
+// File path is relative to root of project, not to this file
+let IndexPage = includeBytes("./src/index.html");
+
+
 
 const myHeaders = new Headers();
 myHeaders.append('CLIENT_SECRET', '241D9EF8EEDFEC7B2D8E213C9DE61');
 myHeaders.append('ACCESS_TOKEN', '13C13122AE89B7E67C7A17DBF6FCA');
-
-let jsonData;
 
 // The entry point for your application.
 //
@@ -19,11 +26,12 @@ let jsonData;
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event) {
+  
   // Get the client request.
   let req = event.request;
 
   // Filter requests that have unexpected methods.
-  if (!["HEAD", "GET"].includes(req.method)) {
+  if (!["HEAD", "GET", "PURGE"].includes(req.method)) {
     return new Response("This method is not allowed", {
       status: 405,
     });
@@ -31,53 +39,22 @@ async function handleRequest(event) {
 
   let url = new URL(req.url);
 
-
-
   // If request is to the `/` path...
   if (url.pathname == "/") {
-    // Below are some common patterns for Compute@Edge services using JavaScript.
-    // Head to https://developer.fastly.com/learning/compute/javascript/ to discover more.
+       
 
-    // Create a new request.
-    //let bereq = new Request("http://api.vin-spritlagret.se/product/618/");
-
-    async function handler(event) {
-      let backendResponse = await fetch("http://api.vin-spritlagret.se/product/618/", {
-        method: "GET",
-        backend: "srv02.vin-spritlagret.se",
-        headers: myHeaders,
-
+    const response = await fetch('http://api.vin-spritlagret.se/product/618/', {
+      backend: "Host_1",
+      headers: myHeaders,
+      method: "GET",
     });
-    jsonData = await backendResponse.json();
-}
+    const body = await response.text();
+    console.log(body);
+    const IndexPage = IndexPage.replace("{JSON}",body.text);
 
-    // Add request headers.
-    //req.headers.set("CLIENT_SECRET", "241D9EF8EEDFEC7B2D8E213C9DE61");
-    // req.headers.set(
-    //   "ACCESS_TOKEN",
-    //   "13C13122AE89B7E67C7A17DBF6FCA"
-    // );
-
-    // Create a cache override.
-    // let cacheOverride = new CacheOverride("override", { ttl: 60 });
-
-    // Forward the request to a backend.
-    // let beresp = await fetch(req, {
-    //   backend: "backend_name",
-    //   cacheOverride,
-    // });
-
-    // Remove response headers.
-    // beresp.headers.delete("X-Another-Custom-Header");
-
-    // Log to a Fastly endpoint.
-    // const logger = fastly.getLogger("my_endpoint");
-    // logger.log("Hello from the edge!");
-
-    indexPage = indexPage.replace("{1_Name}", "Rut & Jollan är bäst!");
-    indexPage = indexPage.replace("{JSON}", JSON.stringify(jsonData, undefined, 2));
+    
     // Send a default synthetic response.
-    return new Response(indexPage, {
+    return new Response(IndexPage, {
       status: 200,
       headers: new Headers({ "Content-Type": "text/html; charset=utf-8" }),
     });
